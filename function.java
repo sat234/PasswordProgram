875bc381627a396ea7e8ad2fe83c285161bcee52
RoboticTactician <132483418+RoboticTactician@users.noreply.github.com>
2023-10-22 16:15:25 -0700

    Update function.java

--- function.java
+++ function.java
@@ -13,14 +13,15 @@ public class function {
     int tally = 0;
     private Set<String> usernames = new HashSet<>();
 
-
+    // Check if the entered username meets the requirements
     public void checkUsername(String userUsername) {
         if (userUsername.length() >= 8 && userUsername.length() <= 16) {
             System.out.println("\nYour username: " + userUsername + " has met the length requirements!");
-    
+
+            // Check if a digit is found in the username
             Pattern digitPattern = Pattern.compile("\\d");
             Matcher digitMatcher = digitPattern.matcher(userUsername);
-    
+
             if (digitMatcher.find()) {
                 System.out.println("\nA digit has been found in " + userUsername + "!");
                 tally++;
@@ -28,10 +29,11 @@ public class function {
                 System.out.println("\nNo digits have been found in " + userUsername + "!");
                 System.exit(0);
             }
-    
+
+            // Check for special characters in the username
             Pattern specialCharPattern = Pattern.compile("[!@#$%&*]");
             Matcher sCharMatcher = specialCharPattern.matcher(userUsername);
-    
+
             if (sCharMatcher.find()) {
                 System.out.println("\nSpecial characters have been found in " + userUsername + "!");
                 tally++;
@@ -39,7 +41,8 @@ public class function {
                 System.out.println("\nNo special characters were found in " + userUsername + "!");
                 System.exit(0);
             }
-    
+
+            // Check if the username is already in use
             if (usernames.contains(userUsername)) {
                 System.out.println("\nThis username is already taken. Please choose a different one.");
                 System.exit(0);
@@ -51,10 +54,8 @@ public class function {
             System.exit(0);
         }
     }
-    
 
-    
-
+    // Check if the entered password meets the requirements
     public void checkPassword(String userPassword) {
         if (userPassword.length() >= 8 && userPassword.length() <= 16) {
             System.out.println("\nYour password: " + userPassword + " has met length requirements!");
@@ -63,6 +64,7 @@ public class function {
             System.exit(0);
         }
 
+        // Check if a digit is found in the password
         Pattern digitPattern = Pattern.compile("\\d");
         Matcher digitMatcher = digitPattern.matcher(userPassword);
 
@@ -74,6 +76,7 @@ public class function {
             System.exit(0);
         }
 
+        // Check for special characters in the password
         Pattern specialCharPattern = Pattern.compile("[!@#$%&*]");
         Matcher sCharMatcher = specialCharPattern.matcher(userPassword);
 
@@ -85,21 +88,23 @@ public class function {
             System.exit(0);
         }
 
+        // Create a file to store usernames and passwords
         createFile(usernames, userPassword);
     }
 
+    // Create a file for storing usernames and passwords
     public void createFile(Set<String> usernames, String password) {
         try {
             File file = new File("Storage.txt");
-    
+
             if (file.createNewFile()) {
                 System.out.println("\nFile created: " + file.getName());
             } else {
                 System.out.println("\nFile already exists.");
             }
-    
+
             FileWriter writer = new FileWriter("Storage.txt", true);
-    
+
             // Read existing usernames from the file
             Set<String> existingUsernames = new HashSet<>();
             Scanner scanner = new Scanner(file);
@@ -111,7 +116,8 @@ public class function {
                 }
             }
             scanner.close();
-    
+
+            // Check if the username is already in use, and write to the file if it's not
             for (String username : usernames) {
                 if (existingUsernames.contains(username)) {
                     System.out.println("\nUsername '" + username + "' already exists. Please choose a different one.");
@@ -119,12 +125,11 @@ public class function {
                     writer.write(username + ":" + password + "\n");
                 }
             }
-    
+
             writer.close();
         } catch (IOException e) {
             System.out.println("\nAn error occurred.");
             e.printStackTrace();
         }
     }
-    
 }
